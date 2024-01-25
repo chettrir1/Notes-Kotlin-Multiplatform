117896cc2e86a86d142ae1f65619664d0e94986d
puja110 <csta.puja@gmail.com>
2024-01-25 18:42:23 -0500

    viewmodel updated

--- androidApp/src/main/java/com/example/noteskmm/android/note_list/NoteListViewModel.kt
+++ androidApp/src/main/java/com/example/noteskmm/android/note_list/NoteListViewModel.kt
@@ -10,12 +10,13 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class NoteListViewModel @Inject constructor(
     private val noteDataSource: NoteDataSource,
-    savedStateHandle: SavedStateHandle
+    private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
     private val searchNotes = SearchNotes()
 
@@ -31,4 +32,27 @@ class NoteListViewModel @Inject constructor(
         )
     }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), NoteListState())
 
+    fun loadNotes() {
+        viewModelScope.launch {
+            savedStateHandle["notes"] = noteDataSource.getAllNote()
+        }
+    }
+
+    fun onSearchTextChange(text: String) {
+        savedStateHandle["searchText"] = text
+    }
+
+    fun onToggleSearch() {
+        savedStateHandle["isSearchActive"] = !isSearchActive.value
+        if (!isSearchActive.value) {
+            savedStateHandle["searchText"] = ""
+        }
+    }
+
+    fun deleteNoteById(id: Long) {
+        viewModelScope.launch {
+            noteDataSource.deleteNoteById(id)
+            loadNotes()
+        }
+    }
 }
